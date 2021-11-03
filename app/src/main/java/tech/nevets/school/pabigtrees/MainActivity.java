dfc1fc2fbed60209099654c1ad5082afdd9b6905
Steven Tracey <steven@nevets.tech>
2021-11-03 00:35:07 -0400

    Final

---
+++ app/src/main/java/tech/nevets/school/pabigtrees/MainActivity.java
@@ -0,0 +1,33 @@
+package tech.nevets.school.pabigtrees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/maps/d/embed?mid=1La6-BnwentXnVHErI39FQrIjyaroGVu1");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
